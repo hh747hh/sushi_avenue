fbb8df74bda5e1f8d6b80191f4e003c9e9a523cf
Woo Yun <hh747hh@gmail.com>
2016-06-03 12:10:28 -0400

    Set up basic Angular Router

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,25 @@
 
 (function(){
   angular
-  .module("sushiAvenue", []);
+  .module("sushiAvenue", [
+    "ui.router"
+
+  ])
+  .config(Router);
+
+  Router.$inject = ["$stateProvider", "$locationProvider", "$urlRouterProvider"];
+  function Router($stateProvider, $locationProvider,$urlRouterProvider){
+    $locationProvider.html5Mode(true);
+    $stateProvider
+    .state("main",{
+      url: "/",
+      template: "<h2>This is working<h2>"
+    })
+    .state("test", {
+      url: "/test",
+      template: "<h2> How can we help you?</h2>"
+    });
+    $urlRouterProvider.otherwise("/");
+  }
 
 })();
