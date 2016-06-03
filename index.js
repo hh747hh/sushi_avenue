0ef85b44282fb558719dcbc16ba5c7a11e7326fb
Woo Yun <hh747hh@gmail.com>
2016-06-03 16:25:25 -0400

    Can POST new Product to database

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 var express = require("express");
+var parser  = require("body-parser");
 var mongoose = require("./db/connection");
 var app     = express();
 
 var Product  = mongoose.model("Product");
 
+app.use(parser.json({urlencoded: true}));
 app.use("/", express.static("public"));
 app.use("/", express.static("bower_components"));
 
 app.get("/api/products", function(req, res){
   Product.find().then(function(products){
     res.json(products);
-
   });
-
 });
+
+
+app.post("/api/products", function(req, res){
+  Product.create(req.body).then(function(product){
+    res.json(product);
+  });
+});
+
 
 app.get("/*", function(req, res){
   res.sendFile(__dirname + "/public/index.html");
